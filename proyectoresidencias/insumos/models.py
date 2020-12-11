b93f70b34f742e49575eae819a9ea982040635d7
JeimmyP <ruelasjeimmy@gmail.com>
2020-12-11 13:02:22 -0700

    configuracion de app y modelos

--- proyectoresidencias/insumos/models.py
+++ proyectoresidencias/insumos/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Familia(models.Model):
+    nombre = models.CharField(max_length=300, blank=False, default='')
+
+class Articulo(models.Model):
+    familiaId = models.ForeignKey('Familia', on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=300, blank=False, default='')
+    unidad = models.CharField(max_length=100, blank=False, default='')
+    tipo = models.CharField(max_length=50, blank=False, default='')
+    grupo = models.CharField(max_length=100, blank=False, default='')
+    serie = models.CharField(max_length=300, blank=False, default='')
+    parte = models.CharField(max_length=150, blank=False, default='')
+    precio = models.CharField(max_length=100, blank=False, default='')
+    toxico = models.CharField(max_length=100, blank=False, default='')
+    activo =models.BooleanField(default=False)
+
+class Movimiento(models.Model):
+    articuloId = models.ForeignKey('Articulo', on_delete=models.CASCADE)
+    fecha =  models.DateTimeField(auto_now=True)
+    tipo = models.CharField(max_length=50, blank=False, default='')
+    cantidad = models.IntegerField(default='0')
+    importe = models.CharField(max_length=100, blank=False, default='')
+    aplicacion = models.CharField(max_length=100, blank=False, default='')
+    referencia = models.CharField(max_length=500, blank=False, default='')
--- proyectoresidencias/proyectoresidencias/settings.py
+++ proyectoresidencias/proyectoresidencias/settings.py
@@ -39,9 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Django REST framework 
     'rest_framework',
+    # Insumos application 
+    'insumos.apps.InsumosConfig',
+     # CORS
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    # CORS
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8081',
+)
